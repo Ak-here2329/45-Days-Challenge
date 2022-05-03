bc61f6febbfa775b5e113e8baa96f3e214117c93
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2022-05-03 19:25:57 +0530

    Update buySellStocks2.cpp

--- Arrays/Day1/buySellStocks2.cpp
+++ Arrays/Day1/buySellStocks2.cpp
@@ -1,19 +1,15 @@
     int maxProfit(vector<int>& prices) {
         int i = 0, ans = 0, buy = 0, sell = 0, n = prices.size();
-        while(i<n){
-            while(i < n-1 and prices[i] > prices[i+1])
+        while(i<n-1){
+            while(i < n-1 and prices[i] >= prices[i+1])
                 i++;
-            if(i==n-1)
-                break;
             buy = prices[i];
-            i++;
             while(i < n-1 and prices[i] < prices[i+1])
                 i++;
             sell = prices[i];
-            if(buy != -1 and sell != -1)
-                ans += (sell - buy);
-            i++;
-            buy = -1, sell = -1;
+            
+            ans += (sell - buy);
+        
         }
         return ans;
     }
