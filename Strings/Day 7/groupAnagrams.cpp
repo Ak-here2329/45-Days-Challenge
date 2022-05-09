d15e8fdcc7a84d4276b655b11926ba7d0c705951
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2022-05-09 13:02:07 +0530

    Create groupAnagrams.cpp

---
+++ Strings/Day 7/groupAnagrams.cpp
@@ -0,0 +1,14 @@
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>> ans;
+        unordered_map<string, vector<string>> m;
+        
+        for(auto s : strs){
+            string temp = s;
+            sort(temp.begin(), temp.end());
+            m[temp].push_back(s);
+        }
+        
+        for(auto i : m)
+            ans.push_back(i.second);
+        return ans;
+    }
