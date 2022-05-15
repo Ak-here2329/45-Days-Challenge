9c0f890a3078c8596bb9ca3336a4c485a8951a54
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2022-05-15 19:24:07 +0530

    Update makeAllEleEqual.cpp

--- Sorting & Searching/Day 12/makeAllEleEqual.cpp
+++ Sorting & Searching/Day 12/makeAllEleEqual.cpp
@@ -7,7 +7,7 @@ int compute(vector<int> &a, int n, int x){
 
 int minCost(vector<int> a, int n){
 	int low = *min_element(a.begin(), a.end()), high = *max_element(a.begin(), a.end());
-	while((low - high) > 2){
+	while((high - low) > 2){
 		int mid1 = low + (high - low) / 3, mid2 = high - (high - low) / 3;
 		int cost1 = compute(a, n, mid1), cost2 = compute(a, n ,mid2);
 		if(cost1 < cost2)
