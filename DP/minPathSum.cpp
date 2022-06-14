321468ed7fda1dd764b6b5e504e0f18f53e35d2e
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2022-06-14 13:59:37 +0530

    Create minPathSum.cpp

---
+++ DP/minPathSum.cpp
@@ -0,0 +1,15 @@
+    int minPathSum(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();
+        vector<int> curr(n, grid[0][0]);
+        
+        for(int i=1; i<n; i++)
+            curr[i] = curr[i-1] + grid[0][i];
+        
+        for(int i=1; i<m; i++){
+            curr[0] += grid[i][0];
+            for(int j=1; j<n; j++){
+                curr[j] = min(curr[j], curr[j-1]) + grid[i][j];
+            }
+        }
+        return curr[n-1];
+    }
