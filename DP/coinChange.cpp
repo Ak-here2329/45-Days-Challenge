e3dc74f2ccd6354ba7b82f3d371f0e966da54fe2
Ak-here2329 <71898228+Ak-here2329@users.noreply.github.com>
2022-06-14 14:02:53 +0530

    Create coinChange.cpp

---
+++ DP/coinChange.cpp
@@ -0,0 +1,13 @@
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, INT_MAX);
+        dp[0] = 0;
+        int n = coins.size();
+        for(int i=1; i<=amount; i++){
+            for(int j=0; j<n; j++){
+                if(i-coins[j] >= 0 and dp[i-coins[j]] != INT_MAX)
+                    dp[i] = min(dp[i], 1 + dp[i - coins[j]]);
+            }
+        }
+        if(dp[amount] == INT_MAX)   return -1;
+        return dp[amount];
+    }
